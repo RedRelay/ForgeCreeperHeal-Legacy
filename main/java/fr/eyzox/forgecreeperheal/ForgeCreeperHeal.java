bcdab380ad881c291fe31bea5d1515ba1c0188b0
Alexandre DUPONCHEL <alexandre.duponchel@gmail.com>
2015-07-31 01:08:08 +0200

    Update version

--- main/java/fr/eyzox/forgecreeperheal/ForgeCreeperHeal.java
+++ main/java/fr/eyzox/forgecreeperheal/ForgeCreeperHeal.java
@@ -26,7 +26,7 @@ public class ForgeCreeperHeal
 {
 	
     public static final String MODID = "forgecreeperheal";
-    public static final String VERSION = "1.1.1";
+    public static final String VERSION = "1.2.0";
     public static final String MODNAME = "Forge Creeper Heal "+VERSION;
     
     @SidedProxy(clientSide = "fr.eyzox.forgecreeperheal.proxy.ClientProxy", serverSide = "fr.eyzox.forgecreeperheal.proxy.CommonProxy")
