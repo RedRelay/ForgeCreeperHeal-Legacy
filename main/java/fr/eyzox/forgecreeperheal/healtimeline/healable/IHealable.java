1599febfe055468c792236d52304bd9933e21e66
Alexandre DUPONCHEL <alexandre.duponchel@gmail.com>
2015-10-01 23:11:09 +0200

    Adds serialization method

--- main/java/fr/eyzox/forgecreeperheal/healtimeline/healable/IHealable.java
+++ main/java/fr/eyzox/forgecreeperheal/healtimeline/healable/IHealable.java
@@ -1,5 +1,6 @@
 package fr.eyzox.forgecreeperheal.healtimeline.healable;
 
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.event.world.ExplosionEvent;
@@ -10,4 +11,6 @@ import fr.eyzox.timeline.ITimelineElement;
 
 public interface IHealable {
 	void heal(World world, int flags);
+	void writeToNBT(NBTTagCompound tag);
+	void readFromNBT(NBTTagCompound tag);
 }
