df02fb24854082df595d808712edd9cca8296c7e
Sam Bassett <samson.bassett@gmail.com>
2016-06-07 16:26:36 -0700

    version number update from 1.1.1 to 1.3.0

--- main/java/fr/eyzox/forgecreeperheal/ForgeCreeperHeal.java
+++ main/java/fr/eyzox/forgecreeperheal/ForgeCreeperHeal.java
@@ -28,8 +28,8 @@ public class ForgeCreeperHeal
 {
 	
     public static final String MODID = "forgecreeperheal";
-    public static final String VERSION = "1.1.1";
-    public static final String MODNAME = "Forge Creeper Heal "+VERSION;
+    public static final String VERSION = "1.3.0";
+    public static final String MODNAME = "Forge Creeper Heal";
     
     @SidedProxy(clientSide = "fr.eyzox.forgecreeperheal.proxy.ClientProxy", serverSide = "fr.eyzox.forgecreeperheal.proxy.CommonProxy")
 	private static CommonProxy proxy;
