472e56f313c4ed0c431f8be6d5215a84fc8a5ff0
Sam Bassett <samson.bassett@gmail.com>
2016-06-07 21:52:14 -0700

    profiler command give info if bad args

--- main/java/fr/eyzox/forgecreeperheal/commands/ProfilerCommand.java
+++ main/java/fr/eyzox/forgecreeperheal/commands/ProfilerCommand.java
@@ -28,85 +28,88 @@ public class ProfilerCommand extends CommandBase{
 
 	@Override
 	public void execute(MinecraftServer server,ICommandSender sender, String[] args) {
-		if(args.length > 0 && args[0] != null) {
-			
-			WorldHealer w = null;
-			int startIndex = 1;
-			boolean switchProfiler = false, force = false, all = false;
-			
-			//Gets switchProfiler
-			if(args[0].equalsIgnoreCase("enable")) {switchProfiler = true;}
-			else if(args[0].equalsIgnoreCase("disable")){ switchProfiler = false;}
-			else{
-				CommonProxy.addChatMessage(sender, this.getCommandUsage(sender));
+		if(args == null || args.length == 0 || args[0] == null) {
+			CommonProxy.addChatMessage(sender, this.getCommandUsage(sender));
+			return;
+		}
+		
+		WorldHealer w = null;
+		int startIndex = 1;
+		boolean switchProfiler = false, force = false, all = false;
+		
+		//Gets switchProfiler
+		if(args[0].equalsIgnoreCase("enable")) {switchProfiler = true;}
+		else if(args[0].equalsIgnoreCase("disable")){ switchProfiler = false;}
+		else{
+			CommonProxy.addChatMessage(sender, this.getCommandUsage(sender));
+		}
+		//Gets all
+		for(int i = startIndex; i<args.length ; i++) {
+			if("all".equalsIgnoreCase(args[i])){
+				all = true;
+				break;
 			}
-			//Gets all
-			for(int i = startIndex; i<args.length ; i++) {
-				if("all".equalsIgnoreCase(args[i])){
-					all = true;
-					break;
-				}
-			}
-			
-			//Gets WorldHealer
-			if(!all) {
-				if(args.length > startIndex) {
-					int dimensionID;
-					try {
-						dimensionID = Integer.parseInt(args[startIndex]);
-					}catch(NumberFormatException e) {
-						CommonProxy.addChatMessage(sender, "command.profiler.dimension");
-						return;
-					}
-					startIndex++;
-					w = ForgeCreeperHeal.getWorldHealer((WorldServer) DimensionManager.getWorld(dimensionID));
-				}else {
-					w = ForgeCreeperHeal.getWorldHealer((WorldServer) sender.getEntityWorld());
-				}
-				
-				if(w == null) {
+		}
+		
+		//Gets WorldHealer
+		if(!all) {
+			if(args.length > startIndex) {
+				int dimensionID;
+				try {
+					dimensionID = Integer.parseInt(args[startIndex]);
+				}catch(NumberFormatException e) {
 					CommonProxy.addChatMessage(sender, "command.profiler.dimension");
 					return;
 				}
-			}
-			
-			//Gets force
-			if(!switchProfiler) {
-				for(int i = startIndex; i<args.length ; i++) {
-					if("force".equalsIgnoreCase(args[i])) {
-						force = true;
-						break;
-					}
-				}
-			}
-			
-
-
-			//Process enable
-			if(switchProfiler) {
-				if(all) {
-					for(WorldServer world : DimensionManager.getWorlds()) {
-						WorldHealer wh = ForgeCreeperHeal.getWorldHealer(world);
-						if(wh != null) wh.enableProfiler(sender);
-					}
-				}else {
-					w.enableProfiler(sender);
-				}
-			//Process disable
+				startIndex++;
+				w = ForgeCreeperHeal.getWorldHealer((WorldServer) DimensionManager.getWorld(dimensionID));
 			}else {
-				if(all) {
-					for(WorldServer world : DimensionManager.getWorlds()) {
-						WorldHealer wh = ForgeCreeperHeal.getWorldHealer(world);
-						if(wh != null) {
-							if(force) wh.disableProfiler();
-							else wh.disableProfiler(sender);
-						}
-					}
-				}else {
-					if(force) w.disableProfiler();
-					else w.disableProfiler(sender);
+				w = ForgeCreeperHeal.getWorldHealer((WorldServer) sender.getEntityWorld());
+			}
+			
+			if(w == null) {
+				CommonProxy.addChatMessage(sender, "command.profiler.dimension");
+				return;
+			}
+		}
+		
+		//Gets force
+		if(!switchProfiler) {
+			for(int i = startIndex; i<args.length ; i++) {
+				if("force".equalsIgnoreCase(args[i])) {
+					force = true;
+					break;
 				}
 			}
 		}
+		
+
+
+		//Process enable
+		if(switchProfiler) {
+			if(all) {
+				for(WorldServer world : DimensionManager.getWorlds()) {
+					WorldHealer wh = ForgeCreeperHeal.getWorldHealer(world);
+					if(wh != null) wh.enableProfiler(sender);
+				}
+			}else {
+				w.enableProfiler(sender);
+			}
+		//Process disable
+		}else {
+			if(all) {
+				for(WorldServer world : DimensionManager.getWorlds()) {
+					WorldHealer wh = ForgeCreeperHeal.getWorldHealer(world);
+					if(wh != null) {
+						if(force) wh.disableProfiler();
+						else wh.disableProfiler(sender);
+					}
+				}
+			}else {
+				if(force) w.disableProfiler();
+				else w.disableProfiler(sender);
+			}
+		}
+	
 	}
 }
