15b96e219dca100e7e36309d38bc2c6c056605fd
Alexandre DUPONCHEL <alexandre.duponchel@gmail.com>
2016-07-25 18:50:00 +0200

    Replace ISerializableHealable

--- main/java/fr/eyzox/forgecreeperheal/builder/ISerializableHealableBuilder.java
+++
@@ -1,9 +0,0 @@
-package fr.eyzox.forgecreeperheal.builder;
-
-import fr.eyzox.forgecreeperheal.factory.IData;
-import fr.eyzox.forgecreeperheal.serial.ISerializableHealable;
-import net.minecraft.nbt.NBTTagCompound;
-
-public interface ISerializableHealableBuilder<KEY> extends IData<KEY>{
-	public ISerializableHealable create(final NBTTagCompound tag);
-}
--- main/java/fr/eyzox/forgecreeperheal/healer/scheduler/IScheduler.java
+++
@@ -1,7 +0,0 @@
-package fr.eyzox.forgecreeperheal.healer.scheduler;
-
-public interface IScheduler<E> {
-	public E next();
-	public boolean hasNext();
-	
-}
