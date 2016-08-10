7fa3c7808083fd3f59b8cd20ac40104906817486
Alexandre DUPONCHEL <alexandre.duponchel@gmail.com>
2016-08-10 02:26:42 +0200

    FIX : Items droped when min&max tick == 0

--- main/java/fr/eyzox/forgecreeperheal/healer/IHealable.java
+++ main/java/fr/eyzox/forgecreeperheal/healer/IHealable.java
@@ -1,7 +1,5 @@
 package fr.eyzox.forgecreeperheal.healer;
 
-import net.minecraft.world.World;
-
 public interface IHealable {
-	void heal(World world, int flags);
+	void heal(WorldHealer worldHealer);
 }
